0ad148b1f1514fa0e81700710a3ebbd77a0458ec
Zach Walsh <zach.c.walsh@gmail.com>
2024-08-09 21:11:01 -0400

    Create NavView component

--- src/main/kotlin/sh/zachwal/dailygames/home/views/HomeView.kt
+++ src/main/kotlin/sh/zachwal/dailygames/home/views/HomeView.kt
@@ -6,21 +6,22 @@ import kotlinx.html.div
 import kotlinx.html.h1
 import kotlinx.html.head
 import kotlinx.html.title
+import sh.zachwal.dailygames.nav.NavView
 import sh.zachwal.dailygames.shared_html.HTMLView
-import sh.zachwal.dailygames.shared_html.favicon
+import sh.zachwal.dailygames.shared_html.darkMode
 import sh.zachwal.dailygames.shared_html.headSetup
-import sh.zachwal.dailygames.shared_html.mobileUI
-import sh.zachwal.dailygames.shared_html.sentryScript
 
 data class HomeView(
     private val name: String
-): HTMLView<HTML>() {
+) : HTMLView<HTML>() {
     override fun HTML.render() {
         head {
             title("Daily Games")
             headSetup()
         }
         body {
+            darkMode()
+            NavView.renderIn(this)
             div(classes = "container") {
                 h1 {
                     +"Hello, world! (test)"
---
+++ src/main/kotlin/sh/zachwal/dailygames/nav/NavView.kt
@@ -0,0 +1,41 @@
+package sh.zachwal.dailygames.nav
+
+import kotlinx.html.BODY
+import kotlinx.html.UL
+import kotlinx.html.a
+import kotlinx.html.div
+import kotlinx.html.header
+import kotlinx.html.i
+import kotlinx.html.li
+import kotlinx.html.span
+import kotlinx.html.ul
+import sh.zachwal.dailygames.shared_html.HTMLView
+
+object NavView : HTMLView<BODY>() {
+    override fun BODY.render() {
+        header(classes = "px-3 py-3 mb-4 border-bottom") {
+            div(classes = "container") {
+                div(classes = "d-flex flex-wrap align-items-center justify-content-center") {
+                    a(href = "/", classes = "nav-link") {
+                        i(classes = "bi bi-globe-europe-africa fs-1 m-2")
+                        span(classes = "lead align-text-bottom") { +"Daily Games" }
+                    }
+                    ul(classes = "nav justify-content-center my-md-0 text-small") {
+                        navItem(href = "/", icon = "bi-house-door-fill", text = "Home")
+                        navItem(href = "/", icon = "bi-bar-chart-fill", text = "Leaderboard")
+                        navItem(href = "/", icon = "bi-person-circle", text = "Profile")
+                    }
+                }
+            }
+        }
+    }
+
+    private fun UL.navItem(href: String, icon: String, text: String) {
+        li {
+            a(href = href, classes = "nav-link text-secondary text-small") {
+                i(classes = "bi $icon d-block text-center fs-3")
+                +text
+            }
+        }
+    }
+}
