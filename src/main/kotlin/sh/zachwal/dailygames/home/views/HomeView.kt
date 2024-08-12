d92abd4f82d3cc69a2c811391950f4a144382b56
Zach Walsh <zach.c.walsh@gmail.com>
2024-08-12 12:09:35 -0400

    HomeView correct UI

--- src/main/kotlin/sh/zachwal/dailygames/home/views/HomeView.kt
+++ src/main/kotlin/sh/zachwal/dailygames/home/views/HomeView.kt
@@ -1,14 +1,27 @@
 package sh.zachwal.dailygames.home.views
 
+import kotlinx.html.FormMethod.post
 import kotlinx.html.HTML
+import kotlinx.html.a
 import kotlinx.html.body
 import kotlinx.html.div
+import kotlinx.html.form
 import kotlinx.html.h1
 import kotlinx.html.head
+import kotlinx.html.label
+import kotlinx.html.li
+import kotlinx.html.p
+import kotlinx.html.passwordInput
+import kotlinx.html.style
+import kotlinx.html.submitInput
+import kotlinx.html.textArea
+import kotlinx.html.textInput
 import kotlinx.html.title
+import kotlinx.html.ul
 import sh.zachwal.dailygames.nav.NavItem
 import sh.zachwal.dailygames.nav.NavView
 import sh.zachwal.dailygames.shared_html.HTMLView
+import sh.zachwal.dailygames.shared_html.card
 import sh.zachwal.dailygames.shared_html.darkMode
 import sh.zachwal.dailygames.shared_html.headSetup
 
@@ -27,8 +40,59 @@ data class HomeView(
             darkMode()
             nav.renderIn(this)
             div(classes = "container") {
-                h1 {
-                    +"Hello, world! (test)"
+                div(classes = "row") {
+                    div(classes = "col") {
+                        card(cardTitle = "Submit Game", cardTitleClasses = "text-center", classes = "mx-3") {
+                            form(method = post, classes = "mb-3") {
+                                div(classes = "mb-3") {
+                                    textArea(classes = "form-control", rows = "9") {
+                                        placeholder = "e.g. #Worldle #123 (10.08.2024) 4/6 (100%)..."
+                                    }
+                                }
+                                submitInput(classes = "btn btn-primary") {
+                                    value = "Submit"
+                                }
+                            }
+                        }
+                    }
+                }
+                div(classes = "row") {
+                    div(classes = "col") {
+                        card(cardTitle = "Games") {
+                            ul {
+                                li {
+                                    a(href = "https://worldle.teuteuf.fr/") { +"Worldle" }
+                                }
+                            }
+                        }
+                    }
+                }
+                div(classes = "row mt-4 border-top") {
+                    div(classes = "col") {
+                        h1(classes = "text-center mt-2 text-decoration-underline") {
+                            +"Feed"
+                        }
+                    }
+                }
+                div(classes = "row") {
+                    div(classes = "col") {
+                        card(
+                            cardHeader = "zach's #Worldle #934",
+                            cardHeaderClasses = "fs-5",
+                            classes = "mx-3 mt-2 mb-4"
+                        ) {
+                            p {
+                                style = "white-space: pre-wrap;"
+                                +"""
+                                #Worldle #934 (12.08.2024) 4/6 (100%)
+                                🟩🟩🟩🟩🟨⬅️
+                                🟩🟩🟩🟩🟨⬅️
+                                🟩🟩🟩🟩🟨↗️
+                                🟩🟩🟩🟩🟩🎉
+                            """.trimIndent()
+                            }
+                        }
+                    }
                 }
             }
         }
