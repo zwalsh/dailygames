e721f1c03595fb4b358e9b341e2ad9938cc48161
zwalsh <46062132+zwalsh@users.noreply.github.com>
2024-08-11 21:18:05 -0400

    Add container to correct centering on HeroView (#9)

--- src/main/kotlin/sh/zachwal/dailygames/home/views/HeroView.kt
+++ src/main/kotlin/sh/zachwal/dailygames/home/views/HeroView.kt
@@ -21,25 +21,27 @@ object HeroView : HTMLView<HTML>() {
         }
         body {
             darkMode()
-            div(classes = "row align-items-center vh-100 vw-100") {
-                div(classes = "px-4 py-5 my-5 text-center") {
-                    i(classes = "bi bi-globe-europe-africa fs-1") // TODO better icon
-                    h1(classes = "display-5 fw-bold text-body-emphasis") {
-                        +"Daily Games"
-                    }
-                    div(classes = "col-lg-6 mx-auto") {
-                        p(classes = "lead mb-4") {
-                            +"Track & share your performance in daily games like "
-                            a(href = "https://worldle.teuteuf.fr/") { +"Worldle" }
-                            +", "
-                            a(href = "https://games.oec.world/en/tradle/") { +"Tradle" }
-                            +", "
-                            a(href = "https://top5-game.com/") { +"Top 5" }
-                            +", and more."
+            div(classes = "container") {
+                div(classes = "row align-items-center vh-100 vw-100") {
+                    div(classes = "px-4 py-5 my-5 text-center") {
+                        i(classes = "bi bi-globe-europe-africa fs-1") // TODO better icon
+                        h1(classes = "display-5 fw-bold text-body-emphasis") {
+                            +"Daily Games"
                         }
-                        div(classes = "d-grid gap-2 d-sm-flex justify-content-sm-center px-5") {
-                            a(href = "/register", classes = "btn btn-primary") { +"Register" }
-                            a(href = "/login", classes = "btn btn-secondary") { +"Login" }
+                        div(classes = "col-lg-6 mx-auto") {
+                            p(classes = "lead mb-4") {
+                                +"Track & share your performance in daily games like "
+                                a(href = "https://worldle.teuteuf.fr/") { +"Worldle" }
+                                +", "
+                                a(href = "https://games.oec.world/en/tradle/") { +"Tradle" }
+                                +", "
+                                a(href = "https://top5-game.com/") { +"Top 5" }
+                                +", and more."
+                            }
+                            div(classes = "d-grid gap-2 d-sm-flex justify-content-sm-center px-5") {
+                                a(href = "/register", classes = "btn btn-primary") { +"Register" }
+                                a(href = "/login", classes = "btn btn-secondary") { +"Login" }
+                            }
                         }
                     }
                 }
