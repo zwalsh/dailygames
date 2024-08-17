aba145a81b4b3dc7b3ca6278fca68a0b17a905dd
Zach Walsh <zach.c.walsh@gmail.com>
2024-08-17 13:53:02 -0400

    Styling of submitted time visual

--- src/main/kotlin/sh/zachwal/dailygames/home/views/ResultFeedItemView.kt
+++ src/main/kotlin/sh/zachwal/dailygames/home/views/ResultFeedItemView.kt
@@ -2,6 +2,8 @@ package sh.zachwal.dailygames.home.views
 
 import kotlinx.html.DIV
 import kotlinx.html.div
+import kotlinx.html.h1
+import kotlinx.html.p
 import kotlinx.html.span
 import kotlinx.html.style
 import sh.zachwal.dailygames.shared_html.HTMLView
@@ -15,14 +17,21 @@ data class ResultFeedItemView(
     override fun DIV.render() {
         div(classes = "row mt-2 mb-4") {
             div(classes = "col") {
-                card(
-                    cardHeader = "$username's $resultTitle",
-                    cardHeaderClasses = "fs-5",
-                    classes = "mx-3"
-                ) {
-                    span {
-                        style = "white-space: pre-wrap;"
-                        +shareText
+                div(classes = "card mx-3") {
+                    div(classes = "card-header") {
+                        h1(classes = "fs-5 my-1") {
+                            +"$username's $resultTitle"
+                        }
+                        p(classes = "text-secondary mb-0") {
+                            style = "font-size: 0.9rem;"
+                            +"2024-08-12 12:34"
+                        }
+                    }
+                    div(classes = "card-body") {
+                        span {
+                            style = "white-space: pre-wrap;"
+                            +shareText
+                        }
                     }
                 }
             }
