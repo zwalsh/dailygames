d9c930fb70d3934b71659387e6fd1816ca038f21
zwalsh <46062132+zwalsh@users.noreply.github.com>
2024-08-17 16:00:39 -0400

    Lengthen session to one month (#19)
    Fix the session expiration so the database task doesn't delete them so
    aggressively. Lengthen the cookie max age & the db record max age to one
    month.

--- src/main/kotlin/sh/zachwal/dailygames/session/DbSessionStorage.kt
+++ src/main/kotlin/sh/zachwal/dailygames/session/DbSessionStorage.kt
@@ -12,7 +12,6 @@ import org.slf4j.LoggerFactory
 import sh.zachwal.dailygames.db.dao.SessionDAO
 import sh.zachwal.dailygames.db.jdbi.Session
 import java.time.Instant
-import java.time.temporal.ChronoUnit
 import javax.inject.Inject
 
 class DbSessionStorage @Inject constructor(private val sessionDAO: SessionDAO) : SessionStorage {
@@ -41,7 +40,7 @@ class DbSessionStorage @Inject constructor(private val sessionDAO: SessionDAO) :
             val bytes = writer {
                 provider(channel)
             }.channel
-            sessionDAO.createOrUpdateSession(Session(id, bytes.toByteArray(), Instant.now().plus(1, ChronoUnit.HOURS)))
+            sessionDAO.createOrUpdateSession(Session(id, bytes.toByteArray(), Instant.now().plus(SESSION_MAX_DURATION)))
         }
     }
 }
