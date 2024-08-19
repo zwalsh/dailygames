26b5606d2b46ad90df94e75b2c14795f939ecddd
Zach Walsh <zach.c.walsh@gmail.com>
2024-08-18 22:04:41 -0400

    Refactor function

--- src/main/resources/static/src/js/leaderboard.js
+++ src/main/resources/static/src/js/leaderboard.js
@@ -1,43 +1,53 @@
-var ctxAllTime = document.getElementById('game-leaderboard-all-time');
-var data = {
-    labels: ['zach', 'derknasty', 'jackiewalsh', 'ChatGPT', 'MikMap'],
-    datasets: [{
-        label: 'Average Score',
-        borderRadius: 5,
-        data: [5.5, 5.3, 4.3, 3.9, 3.2]
-    }]
-};
-var config = {
-    type: 'bar',
-    data: data,
-    options: {
-        plugins: {
-            legend: {
-                display: false
-            }
-        },
-        indexAxis: 'y',
-        aspectRatio: 1.25,
-        scales: {
-            x: {
-                grid: {
+function buildLeaderboardChart(dataLabels, dataValues, elementId) {
+    var ctxAllTime = document.getElementById(elementId);
+    var data = {
+        labels: dataLabels,
+        datasets: [{
+            label: 'Average Score',
+            borderRadius: 5,
+            data: dataValues
+        }]
+    };
+    var config = {
+        type: 'bar',
+        data: data,
+        options: {
+            plugins: {
+                legend: {
                     display: false
                 }
             },
-            y: {
-                grid: {
-                    display: false
+            indexAxis: 'y',
+            aspectRatio: 1.25,
+            scales: {
+                x: {
+                    grid: {
+                        display: false
+                    }
                 },
-                ticks: {
-                    mirror: true,
-                    color: "white",
-                    font: {
-                        weight: "bold",
-                        size: 15
+                y: {
+                    grid: {
+                        display: false
+                    },
+                    ticks: {
+                        mirror: true,
+                        color: "white",
+                        font: {
+                            weight: "bold",
+                            size: 15
+                        }
                     }
                 }
             }
         }
-    }
-};
-var chart = new Chart(ctxAllTime, config);
+    };
+    var chart = new Chart(ctxAllTime, config);
+}
+
+
+window.onload = function() {
+
+    var dataLabels = ['zach', 'derknasty', 'jackiewalsh', 'ChatGPT', 'MikMap'];
+    var dataValues = [5.5, 5.3, 4.3, 3.9, 3.2];
+    buildLeaderboardChart(dataLabels, dataValues, 'game-leaderboard-all-time');
+}
