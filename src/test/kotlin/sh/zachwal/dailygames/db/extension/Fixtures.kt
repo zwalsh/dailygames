d1f4b6215c20a8acd79e454455deda95148b1aa3
Zach Walsh <zach.c.walsh@gmail.com>
2024-08-28 18:07:02 -0400

    ChatDAO with all access patterns

--- src/test/kotlin/sh/zachwal/dailygames/db/extension/Fixtures.kt
+++ src/test/kotlin/sh/zachwal/dailygames/db/extension/Fixtures.kt
@@ -3,7 +3,10 @@ package sh.zachwal.dailygames.db.extension
 import org.jdbi.v3.core.Jdbi
 import org.jdbi.v3.sqlobject.kotlin.onDemand
 import sh.zachwal.dailygames.db.dao.UserDAO
+import sh.zachwal.dailygames.db.dao.game.PuzzleDAO
 import sh.zachwal.dailygames.db.jdbi.User
+import sh.zachwal.dailygames.db.jdbi.puzzle.Game
+import sh.zachwal.dailygames.db.jdbi.puzzle.Puzzle
 
 class Fixtures(
     jdbi: Jdbi
@@ -13,10 +16,18 @@ class Fixtures(
     lateinit var zach: User
     lateinit var jackie: User
 
+
+    private val puzzleDAO: PuzzleDAO = jdbi.onDemand()
+    lateinit var worldle123Puzzle: Puzzle
+    lateinit var flagle123Puzzle: Puzzle
+
     fun runFixtures() {
         zach = userDAO.createUser("zach", "hashedPassword")!!.also {
             println("Created user: $it")
         }
         jackie = userDAO.createUser("jackie", "hashedPassword")!!
+
+        worldle123Puzzle = puzzleDAO.insertPuzzle(Puzzle(Game.WORLDLE, 123, null))
+        flagle123Puzzle = puzzleDAO.insertPuzzle(Puzzle(Game.FLAGLE, 123, null))
     }
 }
