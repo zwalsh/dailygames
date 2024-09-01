cc30ee58c6838e4eafd1973df36b56fad8717a5d
Zach Walsh <zach.c.walsh@gmail.com>
2024-09-01 17:21:17 -0400

    look in the home directory

--- db/migrate.sh
+++ db/migrate.sh
@@ -3,9 +3,9 @@
 # This script runs the Liquibase migrations in the db directory.
 # It uses the environment variables specified in the .env file to authenticate to the database.
 
-username=$(find .. -name "*.env" -exec grep -m 1 "DB_USER" {} \; | cut -d '=' -f 2)
-password=$(find .. -name "*.env" -exec grep -m 1 "DB_PASSWORD" {} \; | cut -d '=' -f 2)
-db_name=$(find .. -name "*.env" -exec grep -m 1 "DB_NAME" {} \; | cut -d '=' -f 2)
+username=$(find ~ -name "*.env" -exec grep -m 1 "DB_USER" {} \; | cut -d '=' -f 2)
+password=$(find ~ -name "*.env" -exec grep -m 1 "DB_PASSWORD" {} \; | cut -d '=' -f 2)
+db_name=$(find ~ -name "*.env" -exec grep -m 1 "DB_NAME" {} \; | cut -d '=' -f 2)
 
 liquibase updateSql --url=jdbc:postgresql://localhost:5432/"$db_name" --username="$username" --password="$password" || exit 1
 
