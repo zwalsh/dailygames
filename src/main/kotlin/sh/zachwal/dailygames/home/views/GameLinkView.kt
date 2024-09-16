6717f35bd6fc466028d321215c48b1fa1f210770
Zach Walsh <zach.c.walsh@gmail.com>
2024-09-15 22:19:53 -0400

    Link & arrow icon

--- src/main/kotlin/sh/zachwal/dailygames/home/views/GameLinkView.kt
+++ src/main/kotlin/sh/zachwal/dailygames/home/views/GameLinkView.kt
@@ -1,8 +1,11 @@
 package sh.zachwal.dailygames.home.views
 
 import kotlinx.html.DIV
+import kotlinx.html.a
 import kotlinx.html.button
 import kotlinx.html.div
+import kotlinx.html.i
+import kotlinx.html.role
 import kotlinx.html.style
 import sh.zachwal.dailygames.db.jdbi.puzzle.Game
 import sh.zachwal.dailygames.shared_html.HTMLView
@@ -12,9 +15,11 @@ object GameLinkView : HTMLView<DIV>() {
         div(classes = "row overflow-auto flex-nowrap py-3 my-1") {
             Game.values().forEach { game ->
                 div(classes = "col") {
-                    button(classes = "btn btn-secondary") {
+                    a(href = game.href(), classes = "btn btn-secondary", target = "_blank") {
+                        role = "button"
                         style = "white-space: nowrap;"
                         +"${game.emoji()} ${game.displayName()}"
+                        i(classes = "bi bi-box-arrow-up-right ms-2")
                     }
                 }
             }
