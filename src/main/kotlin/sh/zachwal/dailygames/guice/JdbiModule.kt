0d09c570fef643196fccd516cf2c93716b64f798
Zach Walsh <zach.c.walsh@gmail.com>
2024-11-03 22:05:40 -0500

    Add the jackson plugin with the jackson object mapper

--- src/main/kotlin/sh/zachwal/dailygames/guice/JdbiModule.kt
+++ src/main/kotlin/sh/zachwal/dailygames/guice/JdbiModule.kt
@@ -1,10 +1,13 @@
 package sh.zachwal.dailygames.guice
 
+import com.fasterxml.jackson.databind.ObjectMapper
 import com.google.inject.AbstractModule
 import com.google.inject.Provides
 import com.google.inject.Singleton
 import org.jdbi.v3.core.Jdbi
 import org.jdbi.v3.core.kotlin.KotlinPlugin
+import org.jdbi.v3.jackson2.Jackson2Config
+import org.jdbi.v3.jackson2.Jackson2Plugin
 import org.jdbi.v3.postgres.PostgresPlugin
 import org.jdbi.v3.sqlobject.kotlin.KotlinSqlObjectPlugin
 import org.jdbi.v3.sqlobject.kotlin.onDemand
@@ -28,10 +31,17 @@ class JdbiModule : AbstractModule() {
 
     @Provides
     @Singleton
-    fun jdbi(ds: DataSource): Jdbi {
-        return Jdbi.create(ds).installPlugin(KotlinPlugin())
+    fun jdbi(ds: DataSource, objectMapper: ObjectMapper): Jdbi {
+        val jdbi = Jdbi.create(ds)
+            .installPlugin(KotlinPlugin())
             .installPlugin(PostgresPlugin())
             .installPlugin(KotlinSqlObjectPlugin())
+            .installPlugin(Jackson2Plugin())
+
+        val config = jdbi.getConfig(Jackson2Config::class.java)
+        config.setMapper(objectMapper)
+
+        return jdbi
     }
 
     @Provides
